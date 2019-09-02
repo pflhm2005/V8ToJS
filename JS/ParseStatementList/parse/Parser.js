81ca1687b21263c15f74498fa4603bf75ed5e5f0
Jimmy <407566546@qq.com>
2019-09-03 02:58:31 +0800

    修改注释

--- JS/ParseStatementList/parse/Parser.js
+++ JS/ParseStatementList/parse/Parser.js
@@ -310,15 +310,15 @@ class ParserBase {
           value_beg_pos = this.peek_position();
           /**
            * 这里处理赋值
-           * 大部分情况下这是一个简单右值 从简到繁(源码使用了一个Precedence来进行渐进解析与优先级判定)
-           * Precedence代表该表达式的复杂程度 值越低越复杂 优先级越低
+           * 大部分情况下这是一个简单右值 源码使用了一个Precedence来进行渐进解析与优先级判定
+           * Precedence代表该表达式的复杂程度 值越低表示越复杂(或运算符优先级越低)
            * Precedence = 2 基本处理方法
-           * Precedence = 3 处理三元表达式 由于三元的每一块都是独立的表达式 之间没有直接的逻辑
+           * Precedence = 3 处理三元表达式 每一块都是独立的表达式 之间没有直接的逻辑
            * Precedence >= 4 处理二元表达式、一元表达式、纯字面量
-           * 其中 多元表达式由于存在连续运算 所以均有一个对应的xxxContinuation方法 
+           * 其中 由于运算存在多元的情况 所以均有一个对应的xxxContinuation方法来递归解析
            * 例如a ? b : c ? d : e、a.b.c、a[b][c]等等
            * 
-           * 而二元表达式的分类如下
+           * Precedence >= 4的分类如下
            * (1)单值字面量 null、true、false、1、1.1、1n、'1'
            * (2)一元运算 +1、++a 形如+function(){}、!function(){}会被特殊处理
            * (3)二元运算 'a' + 'b'、1 + 2
@@ -326,7 +326,7 @@ class ParserBase {
            * 等等情况 实在太过繁琐
            * 除了上述情况 被赋值的可能也是一个左值 比如遇到如下的特殊Token
            * import、async、new、this、function、任意标识符等等
-           * 由于左值的解析相当于一个完整的新表达式 因此不必列举出来
+           * 左值的解析相当于一个完整的新表达式
            */
           value = this.ParseAssignmentExpression();
         }
