f6656b21b4a4efdc78d75ad177db9e464c1917e3
Jimmy <407566546@qq.com>
2019-11-16 03:07:32 +0800

    更新文档

--- Token.js
+++ Token.js
@@ -15,7 +15,10 @@ let scanner = new Scanner(stream);
 let currentToken = '';
 // 启动扫描器
 scanner.Initialize();
-// 扫描下一个Token并打印出来
+/**
+ * 扫描下一个Token并打印出来
+ * 注意 不要放入带有插值(即${})的模板字符串 该Token解析较为特殊
+ */
 while(true) {
   currentToken = scanner.Next();
   console.log(currentToken);
