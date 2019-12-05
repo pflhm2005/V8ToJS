57a43080c45f5b602c0606c9b8aa13557d6308ee
pangfeilong <pangfeilong@bytedance.com>
2019-12-05 19:36:28 +0800

    delete

--- enum/Roots.js
+++
@@ -1,1 +0,0 @@
-export const kEmptyFixedArray = 0;
--- execution/Context.js
+++
@@ -1,8 +0,0 @@
-export default class Context {
-  constructor(isolate) {
-    this.isolate = isolate;
-  }
-  GetIsolate() {
-    return this.isolate;
-  }
-}
